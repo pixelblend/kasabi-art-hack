b2791ff399b2ed032e7c134ba13a31cfa95c4da2
Leigh Dodds <leigh@ldodds.com>
2011-09-21 13:16:23 +0100

    Initial impl of storing annotation

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "rubygems"
 require "bundler/setup"
 
 require 'sinatra'
+
 require File.dirname(__FILE__)+'/lib/annotation'
 
 class CultureHack < Sinatra::Base
@@ -13,5 +14,11 @@ class CultureHack < Sinatra::Base
     content_type 'application/json'
     @image = params[:url] || 'http://www.gac.culture.gov.uk/images/standard/17686.jpg'
     Annotation.list(@image).to_json
+  end
+  
+  post '/annotations' do
+    uri = Annotation.save( params )
+    content_type 'application/json'
+    { "annotation_id" => uri.to_s }.to_json
   end
 end
--- lib/annotation.rb
+++ lib/annotation.rb
@@ -1,7 +1,34 @@
 require 'kasabi'
+require 'rdf'
+require 'uuid'
 
 class Annotation
   ATTRIBUTES = %w{top left width height text id}
+  
+  ANNOTATION_NS = RDF::Vocabulary.new("http://www.w3.org/2000/10/annotation-ns#")
+  ARTHACK = RDF::Vocabulary.new("http://kasabi-art-hack.heroku.com/schema#")
+  ID = UUID.new
+  
+  def self.save( params )
+    @dataset = Kasabi::Dataset.new("http://data.kasabi.com/dataset/government-art-collection-annotations", {:apikey => ENV['KASABI_API_KEY']})
+    store = @dataset.store_api_client()
+    uri = RDF::URI.new("http://kasabi-art-hack.heroku.com/annotations/#{ID.generate}")
+    
+    
+    repository = RDF::Repository.new()
+    
+    repository << [ uri, RDF.type, ANNOTATION_NS.Annotation ]
+    repository << [ uri, ANNOTATION_NS.annotates, RDF::URI.new( params[:image] ) ]
+    repository << [ uri, ANNOTATION_NS.body, params[:text] ]
+    repository << [ uri, ARTHACK.top, params[:top] ]
+    repository << [ uri, ARTHACK.width, params[:width] ]
+    repository << [ uri, ARTHACK.height, params[:height] ]
+    repository << [ uri, ARTHACK.left, params[:left] ]
+    repository << [ uri, RDF::DC.subject, RDF::URI.new( params[:subject] ) ] if params[:subject]
+    
+    store.store_data( repository.dump(:rdfxml) )
+    return uri      
+  end
   
   def self.list(image)
     @dataset = Kasabi::Dataset.new("http://data.kasabi.com/dataset/government-art-collection-annotations", {:apikey => ENV['KASABI_API_KEY']})

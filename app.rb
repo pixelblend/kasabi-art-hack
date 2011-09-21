b1a60340b725ed24b00fc5b0d0e39b63549ac090
Dan Nuttall <pixelblend@gmail.com>
2011-09-21 15:47:03 +0100

    Add related action to find artworks featuring the same subject

--- app.rb
+++ app.rb
@@ -33,4 +33,11 @@ class CultureHack < Sinatra::Base
   get '/annotations/:id/?' do
     Annotation.describe(params[:id])
   end
+  
+  get '/related/?' do
+    content_type 'application/json'
+    subject = params[:subject] || 'http://data.kasabi.com/dataset/government-art-collection/sitters/130697'
+    
+    Annotation.related(subject).to_json
+  end
 end
